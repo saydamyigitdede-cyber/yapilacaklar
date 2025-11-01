b760f74fcbe3b244a5256f5afe93a5cab27cf9a0
sydede <saydamyigitdede@gmail.com>
2025-11-01 14:18:07 +0300

    Update settings.py for Render deploy (DEBUG=False, ALLOWED_HOSTS)

--- yapilacaklar/settings.py
+++ yapilacaklar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure--4f#&g-_l!bseqg=1=^vpm9jc5xe=v^g&b$i#e*vx^r1@x9$)u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
